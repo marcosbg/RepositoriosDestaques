77d0686033bca6ddf3a15c8363bf8441a4565c96
marcosbg <marcosbgomes@gmail.com>
2019-05-18 10:39:59 -0300

    Arquitetura da aplicacao

---
+++ Ateliware.RepositoriosDestaques.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ateliware.RepositoriosDestaques.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
